ba2680fe006f051d0a80eb463689a531d092b2c5
siqi <lsqshr@gmail.com>
2016-07-04 12:22:42 +1000

    DandelionStalker

--- rivuletpy/stalkers.py
+++ rivuletpy/stalkers.py
@@ -14,17 +14,17 @@ class Stalker(ABC):
             face /= np.linalg.norm(face)
             self._face = Vector3(face[0], face[1], face[2]) # The directional vector this stalker is facing
         else:
-        	self._face = face
+            self._face = face
 
         self.path = []
 
     @abstractmethod
     def step(self, action, rewardmap):
-    	pass
+        pass
 
     @abstractmethod
     def sample(self, rewardmap):
-    	pass
+        pass
 
     def render(self, viewer):
         # self._face*180/np.pi
@@ -37,45 +37,16 @@ class Stalker(ABC):
         viewer.add_onetime(ln)
 
 
-class RotStalker(Stalker):
-
-    def __init__(self, pos=Point3(0.0, 0.0, 0.0),
-                 face=None, nsonar=30, raylength=10, raydecay=0.7):
-        super(RotStalker, self).__init__(pos, face)
+class SonarStalker(Stalker, ABC):
+    def __init__(self, pos=Point3(0.0, 0.0, 0.0), face=None, nsonar=30, raylength=10, raydecay=0.7):
+        super(SonarStalker, self).__init__(Point3(0.0, 0.0, 0.0), None)
 
         # Initialise the sonars
-        while True:
-            sonarpts = fibonacci_sphere(nsonar*2)
-            self._sonars = [Vector3(p.x, p.y, p.z) for p in sonarpts if p.x > 0]
-            if len(self._sonars) is nsonar:
-                break
-
+        sonarpts = fibonacci_sphere(nsonar)
+        self._sonars = [Vector3(p.x, p.y, p.z) for p in sonarpts]
         self.raylength = raylength
         self._raydecay = raydecay
         self.path = [] # Save the path of this stalker
-
-
-    def step(self, action, rewardmap):
-        # Rotate the face angles
-        vel = Vector3(action[0], action[1], action[2]) 
-        R  = Quaternion.new_rotate_axis(vel.x, Vector3(1, 0, 0))
-        R *= Quaternion.new_rotate_axis(vel.y, Vector3(0, 1, 0))
-        R *= Quaternion.new_rotate_axis(vel.z, Vector3(0, 0, 1))
-        self._face = R * self._face
-
-        # Rotate sonar rays to new direction
-        self._sonars = [R * s for s in self._sonars]
-
-        # Move to new position
-        pos = self.pos.copy()
-        pos += self._face * np.asscalar(action[-1])
-        # print('==face:',self._face)
-
-        if inbound(pos.xyz, rewardmap.shape):
-            self.pos = pos
-        self.path.append(self.pos)
-
-        return self.sample(rewardmap), self.pos
 
 
     def sample(self, rewardmap):
@@ -89,3 +60,63 @@ class RotStalker(Stalker):
                     break;
                 ob[i] += self._raydecay ** j * rewardmap[rx, ry, rz] # TODO: Maybe change the ray sampling to interpolation
         return ob
+
+
+class RotStalker(SonarStalker):
+
+    def __init__(self, pos=Point3(0.0, 0.0, 0.0),
+                 face=None, nsonar=30, raylength=10, raydecay=0.7):
+        super(RotStalker, self).__init__(pos, face, nsonar*2, raylength, raydecay) 
+
+        # Initialise the sonars
+        while True:
+            sonarpts = fibonacci_sphere(nsonar*2) # sonar * 2 since we only use half of the sphere
+            self._sonars = [Vector3(p.x, p.y, p.z) for p in sonarpts if p.x > 0]
+            if len(self._sonars) is nsonar:
+                break
+
+
+    def step(self, action, rewardmap):
+        # Rotate the face angles
+        vel = Vector3(action[0], action[1], action[2]) # Angular velocity
+        dt = action[-1]
+        R  = Quaternion.new_rotate_axis(vel.x, Vector3(1, 0, 0))
+        R *= Quaternion.new_rotate_axis(vel.y, Vector3(0, 1, 0))
+        R *= Quaternion.new_rotate_axis(vel.z, Vector3(0, 0, 1))
+        self._face = R * self._face
+
+        # Rotate sonar rays to new direction
+        self._sonars = [R * s for s in self._sonars]
+
+        # Move to new position
+        pos = self.pos.copy()
+        pos += self._face * np.asscalar(dt)
+        # print('==face:',self._face)
+
+        if inbound(pos.xyz, rewardmap.shape):
+            self.pos = pos
+        self.path.append(self.pos)
+
+        return self.sample(rewardmap), self.pos
+
+
+class DandelionStalker(SonarStalker):
+    def __init__(self, pos=Point3(0.0, 0.0, 0.0),
+             face=None, nsonar=30, raylength=10, raydecay=0.7):
+        super(DandelionStalker, self).__init__(pos, face, nsonar*2, raylength, raydecay) 
+
+
+    def step(self, action, rewardmap):
+        vel = Vector3(action[0], action[1], action[2])
+        dt = action[-1]
+        self._face = vel
+
+        # Move to new position
+        pos = self.pos.copy()
+        pos += self._face * np.asscalar(dt)
+
+        if inbound(pos.xyz, rewardmap.shape):
+            self.pos = pos
+        self.path.append(self.pos)
+
+        return self.sample(rewardmap), self.pos
--- tests/teststalker.py
+++ tests/teststalker.py
@@ -1,20 +1,34 @@
-from rivuletpy.stalkers import RotStalker
+from rivuletpy.stalkers import RotStalker, DandelionStalker 
 from rivuletpy.utils import rendering3
 from euclid import *
 import numpy as np
 
-rot = RotStalker(pos=Point3(200, 200, 100), face=None, nsonar=30, raylength=10, raydecay=0.7)
 viewer = rendering3.Viewer3(400, 400, 400)
 viewer.set_bounds(0, 400, 0, 400, 0, 400)
-reward = np.zeros((200, 200, 200))
+reward = np.random.rand(200, 200, 200)
 
-for i in range(10000):
+stalker = RotStalker(pos=Point3(200, 200, 100), face=None, nsonar=30, raylength=10, raydecay=0.7)
+for i in range(200):
     print('frame %d' % i, end='\r')
     face = np.random.rand(3,) 
     face -= 0.5
     face *= 2
     face /= np.linalg.norm(face)
     action = np.append(face, [1])
-    rot.step(action, reward)
-    rot.render(viewer)
+    stalker.step(action, reward)
+    stalker.render(viewer)
     viewer.render()
+print('==Rot Passed!')
+
+stalker = DandelionStalker(pos=Point3(200, 200, 100), face=None, nsonar=30, raylength=10, raydecay=0.7)
+for i in range(200):
+    print('frame %d' % i, end='\r')
+    face = np.random.rand(3,) 
+    face -= 0.5
+    face *= 2
+    face /= np.linalg.norm(face)
+    action = np.append(face, [1])
+    stalker.step(action, reward)
+    stalker.render(viewer)
+    viewer.render()
+print('==Dandelion Passed')
