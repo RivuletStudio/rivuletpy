f6c1d3cb77e435b63ffbdf8dc3aeb36797cd1058
siqi <lsqshr@gmail.com>
2016-09-22 14:28:24 +1000

    ubuntu step script ready

---
+++ ubuntu_setup.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+# This script will globally setup the dependencies for rivuletpy
+
+# Setup the dependencies for scipy and pip3
+yes | sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran;
+yes | sudo apt-get install python3-pip;
+yes | sudo apt-get install python3-matplotlib; # Matplotlib easier to install from ubuntu repo 
+
+# Install pip packages
+sudo pip3 install numpy # Needs to be installed outside of requirements
+sudo pip3 install scipy # Needs to be installed outside of requirements
+sudo pip3 install Cython # Needs to be installed outside of requirements
+sudo pip3 install git+https://github.com/pearu/pylibtiff.git; # Install pylibtiff from its github master branch
+sudo pip3 install -r requirements.txt; # Install the requirements.txt friendly packages
+
+# Run a quick test
+# sh quicktest.sh
