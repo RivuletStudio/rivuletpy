3653b1a6db33f1285bd3869eb50a0279f149d62e
siqi <lsqshr@gmail.com>
2016-09-22 16:02:46 +1000

    immediately update numpy after pip3 install. other wise stack() was missing

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8,10 +8,8 @@ sudo apt-get -y install python3-matplotlib; # Matplotlib easier to install from 
 
 # Install pip packages
 sudo pip3 install numpy=1.11.1; # Needs to be installed outside of requirements
+sudo pip3 install numpy --upgrade;
 sudo pip3 install scipy; # Needs to be installed outside of requirements
 sudo pip3 install Cython; # Needs to be installed outside of requirements
 sudo pip3 install git+https://github.com/pearu/pylibtiff.git; # Install pylibtiff from its github master branch
 sudo pip3 install -r requirements.txt; # Install the requirements.txt friendly packages
-
-# Run a quick test
-# sh quicktest.sh
