766f74a6a0a64fde4638259e1f80919c2547f7ed
siqi <lsqshr@gmail.com>
2016-10-11 16:40:09 +1100

    remove sudo from pip3

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,12 +7,12 @@ sudo apt-get -y install python3-pip;
 sudo apt-get -y install python3-matplotlib; # Matplotlib easier to install from ubuntu repo 
 
 # Install pip packages
-sudo pip3 install numpy=1.11.1; # Needs to be installed outside of requirements
-sudo pip3 install numpy --upgrade;
-sudo pip3 install scipy; # Needs to be installed outside of requirements
-sudo pip3 install Cython; # Needs to be installed outside of requirements
-sudo pip3 install git+https://github.com/pearu/pylibtiff.git; # Install pylibtiff from its github master branch
-sudo pip3 install -r requirements.txt; # Install the requirements.txt friendly packages
+pip3 install numpy=1.11.1; # Needs to be installed outside of requirements
+pip3 install numpy --upgrade;
+pip3 install scipy; # Needs to be installed outside of requirements
+pip3 install Cython; # Needs to be installed outside of requirements
+pip3 install git+https://github.com/pearu/pylibtiff.git; # Install pylibtiff from its github master branch
+pip3 install -r requirements.txt; # Install the requirements.txt friendly packages
 
 chmod +x rivulet2
 chmod +x tracejson
