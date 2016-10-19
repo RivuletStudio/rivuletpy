1c194ec55e717362f5f297a2e4842256d098f21c
siqi <lsqshr@gmail.com>
2016-10-19 17:11:57 +1100

    modified for deb packaging; merged _msfm.c and _common.c

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ packages = ['rivuletpy', 'rivuletpy.utils', 'filtering']
 
 ext_modules = [Extension('msfm',
                         sources = [join('rivuletpy', 'msfm', 'msfmmodule.c'),
-		                join('rivuletpy', 'msfm', '_msfm.c'),
-                                          ])]
+		    join('rivuletpy', 'msfm', '_msfm.c'),],
+                                          include_dirs=[join('rivuletpy', 'msfm')])]
 
 setup(name = 'rivuletpy',
       version = '0.1',
@@ -15,7 +15,9 @@ setup(name = 'rivuletpy',
       ext_modules = ext_modules,
       include_dirs = [np.get_include()], #Add Include path of numpy
       packages = packages,
-      scripts = ['ubuntu_setup.sh',],
+      scripts = ['rivulet2', 'compareswc', 'tracejson'],
       # url='http://adamlamers.com',
+      # install_requires=["numpy(>=1.0)", "scipy", "Cython", "scikit-fmm", "scikit-image", "progressbar2", "nibabel",],
+      # dependency_links=['git+https://github.com/pearu/pylibtiff.git'],
       author='Siqi Liu',
       author_email='lsqshr at gmail dot com')
