450daed285e4be6c8dab024f90949447793ec9ab
Siqi Liu <lsqshr@users.noreply.github.com>
2016-11-14 10:15:34 +1100

    add comment to online confidence score +1 term

--- rivuletpy/trace.py
+++ rivuletpy/trace.py
@@ -259,6 +259,8 @@ def iterative_backtrack(t,
 
                 # Compute the online confidence
                 online_voxsum += endpt_b
+                # The online confidence (OC) starts from 0.5 due to the +1 term,
+                # It is helpful to make the OC score from noise points decay fast
                 online_confidence = online_voxsum / (len(branch) + 1)
 
                 if np.linalg.norm(
