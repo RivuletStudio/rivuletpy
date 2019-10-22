08e04e40b5db687d3faa402f6c87aa90e123da96
donghaozhang <zdhpeter1991@gmail.com>
2019-10-22 15:39:33 -0400

    Node and edge conversion

--- rivuletpy/utils/graph_helper2.py
+++ rivuletpy/utils/graph_helper2.py
@@ -7,13 +7,6 @@ import collections
 import math
 import sys
 sys.setrecursionlimit(10000)
-swcpath = '/home/donghao/Desktop/Liver_vessel/dh_test/runs/vessel_pred_0.1.swc'
-swc = loadswc(swcpath)
-swc_copy = swc.copy()
-total_node_number = swc_to_total_node_num(swc_array_input=swc_copy)
-tree_map = create_tree_map(swc_array_input=swc_copy,
-                           total_node_number_input=total_node_number)
-swcindex_nodeid_map = create_swcindex_nodeid_map(swc_array_input=swc_copy)
 
 
 def get_node_id_pid(swc_array_row_number_input, swc_array_input):
@@ -23,8 +16,8 @@ def get_node_id_pid(swc_array_row_number_input, swc_array_input):
 
 
 def edge_exist(node_id_input, node_pid_input, tree_map_input):
-    node_id_nonexist = True
-    node_pid_nonexist = True
+    node_id_nonexist = False
+    node_pid_nonexist = False
     if node_id_input in tree_map_input.keys():
         node_id_nonexist = False
     if node_pid_input in tree_map_input.keys():
@@ -32,7 +25,7 @@ def edge_exist(node_id_input, node_pid_input, tree_map_input):
     if node_pid_nonexist == False and node_id_nonexist == False:
         if tree_map[node_id_input] == node_pid_input:
             edge_exist = True
-    if node_id_input == -1 or node_id_nonexist or node_pid_nonexist:
+    if node_id_input == -1 or node_pid_input == -1 or node_id_nonexist or node_pid_nonexist:
         return False
     elif edge_exist:
         return True
@@ -71,13 +64,19 @@ def edge_to_node(swc_array_input, node_id_input, node_pid_input, swcindex_nodeid
     return new_node
 
 
+swcpath = '/home/donghao/Desktop/Liver_vessel/dh_test/runs/vessel_pred_0.1.swc'
+swc = loadswc(swcpath)
+swc_copy = swc.copy()
+total_node_number = swc_to_total_node_num(swc_array_input=swc_copy)
+tree_map = create_tree_map(swc_array_input=swc_copy,
+                           total_node_number_input=total_node_number)
+swcindex_nodeid_map = create_swcindex_nodeid_map(swc_array_input=swc_copy)
 test_row_number = 3
 node_id, node_pid = get_node_id_pid(swc_array_row_number_input=test_row_number,
                                     swc_array_input=swc_copy)
 test_edge_exist = edge_exist(node_id_input=node_id,
                              node_pid_input=node_pid,
                              tree_map_input=tree_map)
-print('The value of test_edge_exist', test_edge_exist)
 newnode_id = 1
 node_x, node_y, node_z = node_id_to_location(swc_array_input=swc_copy,
                                              node_id_input=node_id,
@@ -87,4 +86,37 @@ newnode = edge_to_node(swc_array_input=swc_copy,
                        node_pid_input=node_pid,
                        swcindex_nodeid_map_input=swcindex_nodeid_map,
                        newnode_id_input=newnode_id)
-print('newnode', newnode)
+new_node_counter = 0
+for cur_num in range(total_node_number):
+
+    node_id, node_pid = get_node_id_pid(swc_array_row_number_input=cur_num,
+                                        swc_array_input=swc_copy)
+    test_edge_exist = edge_exist(node_id_input=node_id,
+                                 node_pid_input=node_pid,
+                                 tree_map_input=tree_map)
+    if test_edge_exist:
+        newnode = edge_to_node(swc_array_input=swc_copy,
+                               node_id_input=node_id,
+                               node_pid_input=node_pid,
+                               swcindex_nodeid_map_input=swcindex_nodeid_map,
+                               newnode_id_input=newnode_id)
+        new_node_counter = new_node_counter + 1
+        if new_node_counter == 1:
+            newnode_all = newnode
+        else:
+            newnode_all = np.vstack((newnode_all, newnode))
+#     print('cur_num',
+#           cur_num,
+#           'new_node_counter',
+#           new_node_counter,
+#           'node_id',
+#           node_id,
+#           'node_pid',
+#           node_pid,
+#           'test_edge_exist',
+#           test_edge_exist,
+#           'newnode',
+#           newnode)
+print('Total number of new nodes is', new_node_counter)
+print('The shape of newnode_all', newnode_all.shape)
+print('The total_node_number is', total_node_number)
