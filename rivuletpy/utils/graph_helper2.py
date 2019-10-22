dbd70ef2416a0bd68693f087fd3604f3c0b1d1c4
donghaozhang <zdhpeter1991@gmail.com>
2019-10-22 15:34:09 -0400

    I am working node edge conversion

---
+++ rivuletpy/utils/graph_helper2.py
@@ -0,0 +1,90 @@
+from package.rivuletpy.rivuletpy.utils.io import loadswc
+from package.rivuletpy.rivuletpy.utils.graph_helper import swc_to_total_node_num
+from package.rivuletpy.rivuletpy.utils.graph_helper import create_swcindex_nodeid_map
+from package.rivuletpy.rivuletpy.utils.graph_helper import create_tree_map
+import numpy as np
+import collections
+import math
+import sys
+sys.setrecursionlimit(10000)
+swcpath = '/home/donghao/Desktop/Liver_vessel/dh_test/runs/vessel_pred_0.1.swc'
+swc = loadswc(swcpath)
+swc_copy = swc.copy()
+total_node_number = swc_to_total_node_num(swc_array_input=swc_copy)
+tree_map = create_tree_map(swc_array_input=swc_copy,
+                           total_node_number_input=total_node_number)
+swcindex_nodeid_map = create_swcindex_nodeid_map(swc_array_input=swc_copy)
+
+
+def get_node_id_pid(swc_array_row_number_input, swc_array_input):
+    node_id = int(swc_array_input[swc_array_row_number_input, 0])
+    node_pid = int(swc_array_input[swc_array_row_number_input, 6])
+    return node_id, node_pid
+
+
+def edge_exist(node_id_input, node_pid_input, tree_map_input):
+    node_id_nonexist = True
+    node_pid_nonexist = True
+    if node_id_input in tree_map_input.keys():
+        node_id_nonexist = False
+    if node_pid_input in tree_map_input.keys():
+        node_pid_nonexist = False
+    if node_pid_nonexist == False and node_id_nonexist == False:
+        if tree_map[node_id_input] == node_pid_input:
+            edge_exist = True
+    if node_id_input == -1 or node_id_nonexist or node_pid_nonexist:
+        return False
+    elif edge_exist:
+        return True
+
+
+def node_id_to_location(swc_array_input,
+                        node_id_input,
+                        swcindex_nodeid_map_input):
+    swc_array_row_number = swcindex_nodeid_map_input[node_id_input]
+    node_x = swc_array_input[swc_array_row_number, 2]
+    node_y = swc_array_input[swc_array_row_number, 3]
+    node_z = swc_array_input[swc_array_row_number, 4]
+    return node_x, node_y, node_z
+
+
+def edge_to_node(swc_array_input, node_id_input, node_pid_input, swcindex_nodeid_map_input, newnode_id_input):
+    node1_x, node1_y, node1_z = node_id_to_location(swc_array_input=swc_array_input,
+                                                    node_id_input=node_id_input,
+                                                    swcindex_nodeid_map_input=swcindex_nodeid_map_input)
+    node2_x, node2_y, node2_z = node_id_to_location(swc_array_input=swc_array_input,
+                                                    node_id_input=node_pid_input,
+                                                    swcindex_nodeid_map_input=swcindex_nodeid_map_input)
+    new_node_x = (node1_x + node2_x) / 2
+    new_node_y = (node1_y + node2_y) / 2
+    new_node_z = (node1_z + node2_z) / 2
+    new_node_radius = 1
+    new_node_structure = 1
+    new_node = np.asarray([newnode_id_input,
+                           new_node_structure,
+                           new_node_x,
+                           new_node_y,
+                           new_node_z,
+                           new_node_radius,
+                           node_id_input,
+                           node_pid_input])
+    return new_node
+
+
+test_row_number = 3
+node_id, node_pid = get_node_id_pid(swc_array_row_number_input=test_row_number,
+                                    swc_array_input=swc_copy)
+test_edge_exist = edge_exist(node_id_input=node_id,
+                             node_pid_input=node_pid,
+                             tree_map_input=tree_map)
+print('The value of test_edge_exist', test_edge_exist)
+newnode_id = 1
+node_x, node_y, node_z = node_id_to_location(swc_array_input=swc_copy,
+                                             node_id_input=node_id,
+                                             swcindex_nodeid_map_input=swcindex_nodeid_map)
+newnode = edge_to_node(swc_array_input=swc_copy,
+                       node_id_input=node_id,
+                       node_pid_input=node_pid,
+                       swcindex_nodeid_map_input=swcindex_nodeid_map,
+                       newnode_id_input=newnode_id)
+print('newnode', newnode)
